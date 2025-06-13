ef9c3081af5c01e2dd465e42effe0fc10f29f1b4
nsshan98 <n.sakibnss@gmail.com>
2025-06-13 22:55:25 +0600

    fix(prisma): update database connection URL for local development

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,13 +3,13 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
-    super({
-      datasources: {
-        db: {
-          url: 'postgresql://neondb_owner:npg_tLjZqOUKk81P@ep-icy-rice-a8rboye0-pooler.eastus2.azure.neon.tech/neondb?sslmode=require',
-        },
-      },
-    });
-  }
+    constructor() {
+        super({
+            datasources: {
+                db: {
+                    url: 'postgresql://postgres:password@localhost:5432/nestjs-prisma',
+                },
+            },
+        });
+    }
 }
