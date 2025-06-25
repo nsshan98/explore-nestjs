4499e7cbb8df3accf22b7cfd7a7e41f1361938f5
nsshan98 <n.sakibnss@gmail.com>
2025-06-25 21:57:55 +0600

    feat(prisma): integrate @nestjs/config for database URL configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [AuthModule, UserModule, PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UserModule, PrismaModule],
 })
-export class AppModule {}
+export class AppModule { }
