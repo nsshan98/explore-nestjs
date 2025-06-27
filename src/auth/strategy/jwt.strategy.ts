64ae523fc583eb9072f0937092e2fd7d02c3c388
nsshan98 <n.sakibnss@gmail.com>
2025-06-27 22:44:56 +0600

    feat(auth): implement JWT guard and user controller for user authentication

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,19 +2,31 @@ import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(config: ConfigService) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+    constructor(config: ConfigService, private prisma: PrismaService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: config.get('JWT_SECRET') || 'defaultSecretKey',
         })
     }
 
-    validate(payload: any) {
-        console.log(payload, 'jwt.strategy.ts');
-        return { payload }
+    async validate(payload: { sub: string, email: string }) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: payload.sub,
+            }
+        })
+        if (!user) {
+            return {
+                msg: 'User not found',
+                statusCode: 404,
+            }; // or throw an exception if you prefer
+        }
+        const { password, ...rest } = user;
+        return { ...rest }
     }
 
 }
